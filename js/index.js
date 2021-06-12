90abc339b3d77173edc0d1ce66bdae647949690d
RicaGhosh <ricaghosh172@gmail.com>
2021-06-13 01:25:36 +0530

    FB Login

--- js/index.js
+++ js/index.js
@@ -108,10 +108,19 @@ window.fbAsyncInit = function() {
     
     FB.AppEvents.logPageView();   
     */
-    FB.getLoginStatus(function(response){
-        
-        statusChangeCallback(response);
+    FB.login(function(response){
+        if (response.authResponse){
+            console.log("Welcome! Fetching your information");
+            FB.api('/me', function(response){
+                console.log('Good to see you'+response.name);
+            });
+        } else {
+            console.log('User cancelled login in.')
+        }
     });
+    /*FB.getLoginStatus(function(response){
+        statusChangeCallback(response);
+    });*/
 };
 
 (function(d, s, id){
