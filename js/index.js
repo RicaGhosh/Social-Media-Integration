1798caaf92364c4d0efb761197ed8548195a343e
RicaGhosh <ricaghosh172@gmail.com>
2021-06-14 01:47:13 +0530

    FB Login

--- js/index.js
+++ js/index.js
@@ -76,7 +76,7 @@ fmail.style.visibility = 'hidden';
 flo.style.visibility = 'hidden';
 
 function getUserData(){
-    FB.api('/me', {fields: 'name,email,/{user-id}/picture'}, function(response) {
+    FB.api('/me', {fields: 'name,email,picture'}, function(response) {
         console.log(response)
         console.log(response.data)
         console.log(response.id);
