569496253b35783526a1b22358a906acd8a64d81
RicaGhosh <ricaghosh172@gmail.com>
2021-06-13 22:10:07 +0530

    Modified FB Login css

--- js/index.js
+++ js/index.js
@@ -94,7 +94,7 @@ window.fbAsyncInit = function() {
     var js, fjs = d.getElementsByTagName(s)[0];
     if (d.getElementById(id)) {return;}
     js = d.createElement(s); js.id = id;
-    js.src = "https://connect.facebook.com/en_US/sdk.js";
+    js.src = "https://connect.facebook.net/en_US/sdk.js";
     fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
 
